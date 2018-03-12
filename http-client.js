a999cb99f26644e15666662e75f8c7f7db113a8e
xianshuihu <junhua.libyd@foxmail.com>
2018-03-12 00:38:39 -0700

    init https demo

---
+++ http-client.js
@@ -0,0 +1,26 @@
+var https = reuqire('https');
+var fs = require('fs');
+
+var options = {
+	hostname: 'localhost',
+	port: 8000,
+	path: '/',
+	method: 'GET',
+	key: fs.readFileSync('./keys/client.key'),
+	cert: fs.readFileSync('./keys/client.crt'),
+	ca: [fs.readFileSync('./keys/ca.crt')]
+};
+
+options.agent = new https.Agent(options);
+
+var req = https.request(options, function(res) {
+	res.setEncoding('utf-8');
+	res.on('data', function(d) {
+		console.log(d);
+	});
+});
+req.end();
+
+req.on('error', function(e) {
+	console.log(e);
+});
